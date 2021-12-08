ed43c2a362c7a70b3f30952f4d6bee62ea8b827c
Lawrence Wu <lawrencejwu@gmail.com>
2021-12-08 12:24:38 -0600

    always useContext to avoid react warning about hooks changing on render

--- js/packages/web/src/components/CurrentUserBadge/index.tsx
+++ js/packages/web/src/components/CurrentUserBadge/index.tsx
@@ -143,13 +143,13 @@ export const CurrentUserBadge = (props: {
   const { account } = useNativeAccount();
   const solPrice = useSolPrice();
   const [showAddFundsModal, setShowAddFundsModal] = useState<Boolean>(false);
+  const solMintInfo = useTokenList().tokenMap.get(WRAPPED_SOL_MINT.toString());
 
   if (!wallet || !publicKey) {
     return null;
   }
   const balance = (account?.lamports || 0) / LAMPORTS_PER_SOL;
   const balanceInUSD = balance * solPrice;
-  const solMintInfo = useTokenList().tokenMap.get(WRAPPED_SOL_MINT.toString());
   const iconStyle: React.CSSProperties = {
     display: 'flex',
     width: props.iconSize,
