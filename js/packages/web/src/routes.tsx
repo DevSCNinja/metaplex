df4fed5ef160d6aed39c4bbf3dffa33a1256c32d
Lawrence Wu <lawrencejwu@gmail.com>
2021-12-07 22:03:03 -0600

    lift ingredients and recipe out of fireball view

--- js/packages/web/src/routes.tsx
+++ js/packages/web/src/routes.tsx
@@ -1,16 +1,61 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { PublicKey } from '@solana/web3.js';
+
 import { Providers } from './providers';
 import { AdminView } from './views/admin';
 import { FireballView} from "./views/fireballView";
 
 export function Routes() {
+  const ingredients = {
+    "airplane"           : "https://www.arweave.net/84UaRlQ7lIM6rjGodFsruqNNAoOBt6dBoJ-eHv9Fr50?ext=gif",
+    "bull"               : "https://www.arweave.net/GfSyYWWgOIY3llKsU9CiR_sUKNlIBaE1-Wnx_JgvaC4?ext=gif",
+    "duck with doughnut" : "https://www.arweave.net/4M30mRpOwq9M1DrlMAUipUAaAPsCeMLm8gTSDXo_rmI?ext=gif",
+    "hot air balloon"    : "https://www.arweave.net/_mNWVadW1eA5Be3qJlDJeY5qc5tcfL0VdwJ7mc2oxgU?ext=gif",
+    "house"              : "https://www.arweave.net/StFWkC5bN_vMuY6oluIlJbFMPsCL-6Q93aVCobrA_mM?ext=gif",
+    "normal duck"        : "https://www.arweave.net/PJySMI3c2s-DFvJ_ruRrCScsNJiUDiJsu9J6haeWaww?ext=gif",
+    "rocket"             : "https://www.arweave.net/tWQYjhOarxQbQvF9eGRUnI3S-vaWd8Qj7ag7CmiVRqk?ext=gif",
+    "sailboat"           : "https://www.arweave.net/RIkpf6zSCcFLi6KetJrnwd5feZdlVc9-5E37n58D_H4?ext=gif",
+    "telescope ape"      : "https://www.arweave.net/yxWPmiQY3OBHLn1kWhDOrvuJMNAbkglI3VzrL8xZk1Y?ext=gif",
+    "traincar"           : "https://www.arweave.net/mt_fveAydzly6mEeAUNxDuAWevIe9NPoxBuPoTCDIYY?ext=gif",
+    "ufo"                : "https://www.arweave.net/RNdstwUgOcXc7ognVkUoTjfoO2B3Kp2iZ34m86x6gzw?ext=gif",
+    "umbrella duck"      : "https://www.arweave.net/-ApXoK_X3rlclU-rijXiqU4pm85tggLej4ax3HwsI3U?ext=gif",
+    "whale"              : "https://www.arweave.net/e0VvxBG4VrAmli9v7E0d_JDxqbXohS50D7oExbtzVkg?ext=gif",
+  };
+
+  const cityYields = [
+    {
+      image: "https://4udejsogpoo3ekjmiigzgcgzyebntokt6oqavvw5vsv77xpvp5eq.arweave.net/5QZEycZ7nbIpLEINkwjZwQLZuVPzoArW3ayr_931f0k/?ext=gif",
+      name: "red moon city",
+      mint: new PublicKey("2gFFVAaFQe36FTBevz2cQHivXUfPse53dBSN65e96HND"),
+    },
+    {
+      image: "https://pvibl5h2u52szj5hq5h4pselyzhjsckz53oziusek2insn4f75va.arweave.net/fVAV9PqndSynp4dPx8iLxk6ZCVnu3ZRSRFaQ2TeF_2o/?ext=gif",
+      name: "blue moon beach",
+      mint: new PublicKey("9vpjkWrc4GSW98HgrTZaknHKtxdrx7Cq6P6is4A7uwE1"),
+    },
+    {
+      image: "https://u2pr74tvgu2uxgvscxk22my5b6o5esatoqgcv5npyfaaef37tv3a.arweave.net/pp8f8nU1NUuashXVrTMdD53SSBN0DCr1r8FAAhd_nXY/?ext=gif",
+      name: "once in a solana moon",
+      mint: new PublicKey("ENSkFqG4unsRq6bFa17vngQ8rfxsVdcvJJijyHdFi2XQ"),
+    },
+  ];
+
   return (
     <>
       <BrowserRouter basename={'/'}>
         <Providers>
           <Switch>
-            <Route path="/cities" component={() => <FireballView />} />
+            <Route path="/collectoooooor" component={
+              () => (
+                <FireballView
+                  recipeKey={new PublicKey("HHNbiYDEAJ2PXv5GZXXrn2Ypi1s8CfZK4asgnpg6MSUi")}
+                  recipeYields={cityYields}
+                  ingredients={ingredients}
+                />
+              )
+            } />
           </Switch>
         </Providers>
       </BrowserRouter>
--- js/packages/web/src/views/fireballView/index.tsx
+++ js/packages/web/src/views/fireballView/index.tsx
@@ -420,7 +420,18 @@ enum IngredientView {
   recover = 'recover',
 }
 
+export type Recipe = {
+  image: string,
+  name: string,
+  mint: PublicKey,
+};
+
 export const FireballView = (
+  props : {
+    recipeKey : PublicKey,
+    recipeYields : Array<Recipe>,
+    ingredients : { [key: string]: string },
+  }
 ) => {
   const { endpoint } = useConnectionConfig();
   const connection = React.useMemo(
@@ -429,7 +440,9 @@ export const FireballView = (
   );
   const { wallet, anchorWallet, program } = useAnchorContext();
 
-  const recipeKey = new PublicKey("HHNbiYDEAJ2PXv5GZXXrn2Ypi1s8CfZK4asgnpg6MSUi");
+  const recipeKey = props.recipeKey;
+  const recipes = props.recipeYields;
+  const ingredients = props.ingredients;
 
   const [recipeYields, setRecipeYields] = React.useState<Array<RecipeYield>>([]);
   const [relevantMints, setRelevantMints] = React.useState<Array<RelevantMint>>([]);
@@ -437,40 +450,6 @@ export const FireballView = (
   const [dishIngredients, setIngredients] = React.useState<Array<RelevantMint>>([]);
   const [changeList, setChangeList] = React.useState<Array<any>>([]);
   const [matchingIndices, setMatchingIndices] = React.useState<{ [key: string]: number }>({});
-
-  const recipes = [ // TODO: metadata link to find editions remaining
-    {
-      image: "https://4udejsogpoo3ekjmiigzgcgzyebntokt6oqavvw5vsv77xpvp5eq.arweave.net/5QZEycZ7nbIpLEINkwjZwQLZuVPzoArW3ayr_931f0k/?ext=gif",
-      name: "red moon city",
-      mint: new PublicKey("2gFFVAaFQe36FTBevz2cQHivXUfPse53dBSN65e96HND"),
-    },
-    {
-      image: "https://pvibl5h2u52szj5hq5h4pselyzhjsckz53oziusek2insn4f75va.arweave.net/fVAV9PqndSynp4dPx8iLxk6ZCVnu3ZRSRFaQ2TeF_2o/?ext=gif",
-      name: "blue moon beach",
-      mint: new PublicKey("9vpjkWrc4GSW98HgrTZaknHKtxdrx7Cq6P6is4A7uwE1"),
-    },
-    {
-      image: "https://u2pr74tvgu2uxgvscxk22my5b6o5esatoqgcv5npyfaaef37tv3a.arweave.net/pp8f8nU1NUuashXVrTMdD53SSBN0DCr1r8FAAhd_nXY/?ext=gif",
-      name: "once in a solana moon",
-      mint: new PublicKey("ENSkFqG4unsRq6bFa17vngQ8rfxsVdcvJJijyHdFi2XQ"),
-    },
-  ];
-
-  const ingredients = {
-    "airplane"           : "https://www.arweave.net/84UaRlQ7lIM6rjGodFsruqNNAoOBt6dBoJ-eHv9Fr50?ext=gif",
-    "bull"               : "https://www.arweave.net/GfSyYWWgOIY3llKsU9CiR_sUKNlIBaE1-Wnx_JgvaC4?ext=gif",
-    "duck with doughnut" : "https://www.arweave.net/4M30mRpOwq9M1DrlMAUipUAaAPsCeMLm8gTSDXo_rmI?ext=gif",
-    "hot air balloon"    : "https://www.arweave.net/_mNWVadW1eA5Be3qJlDJeY5qc5tcfL0VdwJ7mc2oxgU?ext=gif",
-    "house"              : "https://www.arweave.net/StFWkC5bN_vMuY6oluIlJbFMPsCL-6Q93aVCobrA_mM?ext=gif",
-    "normal duck"        : "https://www.arweave.net/PJySMI3c2s-DFvJ_ruRrCScsNJiUDiJsu9J6haeWaww?ext=gif",
-    "rocket"             : "https://www.arweave.net/tWQYjhOarxQbQvF9eGRUnI3S-vaWd8Qj7ag7CmiVRqk?ext=gif",
-    "sailboat"           : "https://www.arweave.net/RIkpf6zSCcFLi6KetJrnwd5feZdlVc9-5E37n58D_H4?ext=gif",
-    "telescope ape"      : "https://www.arweave.net/yxWPmiQY3OBHLn1kWhDOrvuJMNAbkglI3VzrL8xZk1Y?ext=gif",
-    "traincar"           : "https://www.arweave.net/mt_fveAydzly6mEeAUNxDuAWevIe9NPoxBuPoTCDIYY?ext=gif",
-    "ufo"                : "https://www.arweave.net/RNdstwUgOcXc7ognVkUoTjfoO2B3Kp2iZ34m86x6gzw?ext=gif",
-    "umbrella duck"      : "https://www.arweave.net/-ApXoK_X3rlclU-rijXiqU4pm85tggLej4ax3HwsI3U?ext=gif",
-    "whale"              : "https://www.arweave.net/e0VvxBG4VrAmli9v7E0d_JDxqbXohS50D7oExbtzVkg?ext=gif",
-  };
 
   const numIngredients = Object.keys(ingredients).length;
   const collected = Object.keys(ingredients).reduce((acc, ingredient) => {
@@ -1056,6 +1035,7 @@ export const FireballView = (
     backgroundColor: "#888",
   };
 
+  // TODO: lift wording
   return (
     <Stack
       spacing={1}
@@ -1064,8 +1044,9 @@ export const FireballView = (
       // }}
     >
       <p className={"text-subtitle"}>
-        You can burn 13 NFTs to redeem an exclusive city edition. Click 'MINT' to burn
-        the first 13 ingredients found in your wallet or pick and choose below!
+        You can burn {numIngredients} NFTs to redeem an exclusive city edition.
+        Click 'MINT' to burn the first {numIngredients} ingredients found in
+        your wallet or pick and choose below!
       </p>
       <ImageList cols={cols}>
         {recipes.map((r, idx) => {
